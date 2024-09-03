527338d6e538388d205a6c7000740a5e4d38a506
szb <chamaselion@gmail.com>
2024-09-03 13:49:26 +0200

    Storing the signals first and then receiving to optimize the time delay time. Still not perfect ngl

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 10:42:15 by bszikora          #+#    #+#             */
-/*   Updated: 2024/09/03 11:30:15 by bszikora         ###   ########.fr       */
+/*   Updated: 2024/09/03 13:43:50 by bszikora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,21 @@ void	send_message(int pid, char *message)
 {
 	int	i;
 	int	j;
+	char c;
 
 	i = 0;
 	while (1)
 	{
-		j = 0;
-		while (j < 8)
+		c = message[i];
+		j = 7;
+		while (j >= 0)
 		{
-			if (message[i] & (1 << j))
+			if (c >> j & 1)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
-			usleep(100);
-			j++;
+			usleep(300);
+			j--;
 		}
 		if (message[i] == '\0')
 			break ;
--- server.c
+++ server.c
@@ -6,44 +6,66 @@
 /*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 11:11:47 by bszikora          #+#    #+#             */
-/*   Updated: 2024/09/03 11:33:58 by bszikora         ###   ########.fr       */
+/*   Updated: 2024/09/03 13:47:49 by bszikora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 volatile sig_atomic_t	g_signal;
+volatile sig_atomic_t	bit_count = 0;
+char					bit_buffer[8];
 
 void	sig_handler(int signum)
 {
 	g_signal = signum;
+	if (signum == SIGUSR1)
+	{
+		bit_buffer[bit_count++] = 1;
+	}
+	else if (signum == SIGUSR2)
+	{
+		bit_buffer[bit_count++] = 0;
+	}
 }
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	int		pid;
-	char	c;
-	int		i;
+	int pid;
+	char c;
+	int i;
 
-	pid = getpid();
-	ft_printf("Server PID: %d\n", pid);
-	while (1)
+	if (argc == 1)
 	{
-		c = 0;
-		i = 0;
-		while (i < 8)
+		pid = getpid();
+		printf("Server PID: %d\n", pid);
+
+		signal(SIGUSR1, sig_handler);
+		signal(SIGUSR2, sig_handler);
+
+		while (1)
 		{
-			signal(SIGUSR1, sig_handler);
-			signal(SIGUSR2, sig_handler);
+			if (bit_count >= 8)
+			{
+				c = 0;
+				for (i = 0; i < 8; i++)
+				{
+					c |= (bit_buffer[i] << (7 - i));
+				}
+				write(1, &c, 1);
+				bit_count = 0;
+			}
 			pause();
-			if (g_signal == SIGUSR1)
-				c |= 1 << i;
-			i++;
 		}
-		if (c == '\0')
-			ft_putchar_fd('\n', 1);
-		else
-			ft_putchar_fd(c, 1);
 	}
+	else
+	{
+		printf("Usage: %s\n", argv[0]);
+	}
+
 	return (0);
 }
