66dac7ca8d57f90d3d860670654faa2b0a0ad153
Balint Szikora <bszikora@1-A-22.42heilbronn.de>
2024-09-04 15:34:19 +0200

    Norminettized and changed to only one global variable

--- server.c
+++ server.c
@@ -6,66 +6,66 @@
 /*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 11:11:47 by bszikora          #+#    #+#             */
-/*   Updated: 2024/09/03 13:47:49 by bszikora         ###   ########.fr       */
+/*   Updated: 2024/09/04 15:29:20 by bszikora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include "minitalk.h"
 
-volatile sig_atomic_t	g_signal;
-volatile sig_atomic_t	bit_count = 0;
-char					bit_buffer[8];
+t_sharevalue	g_value;
 
 void	sig_handler(int signum)
 {
-	g_signal = signum;
+	g_value.g_signal = signum;
 	if (signum == SIGUSR1)
 	{
-		bit_buffer[bit_count++] = 1;
+		g_value.bit_buffer[g_value.bit_count++] = 1;
 	}
 	else if (signum == SIGUSR2)
 	{
-		bit_buffer[bit_count++] = 0;
+		g_value.bit_buffer[g_value.bit_count++] = 0;
+	}
+}
+
+void	print_stored_bits(void)
+{
+	int	c;
+	int	i;
+
+	while (1)
+	{
+		if (g_value.bit_count >= 8)
+		{
+			c = 0;
+			i = 0;
+			while (i < 8)
+			{
+				c |= (g_value.bit_buffer[i] << (7 - i));
+				i++;
+			}
+			write(1, &c, 1);
+			if (c == '\0')
+				write(1, "\n", 1);
+			g_value.bit_count = 0;
+		}
+		pause();
 	}
 }
 
 int	main(int argc, char **argv)
 {
-	int pid;
-	char c;
-	int i;
+	int		pid;
 
+	g_value.bit_count = 0;
 	if (argc == 1)
 	{
 		pid = getpid();
-		printf("Server PID: %d\n", pid);
-
+		ft_printf("Server PID: %d\n", pid);
 		signal(SIGUSR1, sig_handler);
 		signal(SIGUSR2, sig_handler);
-
-		while (1)
-		{
-			if (bit_count >= 8)
-			{
-				c = 0;
-				for (i = 0; i < 8; i++)
-				{
-					c |= (bit_buffer[i] << (7 - i));
-				}
-				write(1, &c, 1);
-				bit_count = 0;
-			}
-			pause();
-		}
+		print_stored_bits();
 	}
 	else
-	{
-		printf("Usage: %s\n", argv[0]);
-	}
-
+		ft_printf("Usage: %s\n", argv[0]);
 	return (0);
 }
