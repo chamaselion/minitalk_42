6d4dcb755bb6f0cf6e77a10e8250b48e337cd19b
Balint Szikora <bszikora@1-A-22.42heilbronn.de>
2024-09-04 15:34:42 +0200

    Updated header with struct

--- minitalk.h
+++ minitalk.h
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bszikora <bszikora@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 12:01:27 by bszikora          #+#    #+#             */
+/*   Updated: 2024/09/04 14:55:07 by bszikora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
@@ -6,5 +17,12 @@
 # include <signal.h>
 # include <stdlib.h>
 # include <unistd.h>
+
+typedef struct s_sharevalue
+{
+	volatile sig_atomic_t	g_signal;
+	volatile sig_atomic_t	bit_count;
+	char					bit_buffer[8];
+}	t_sharevalue;
 
 #endif
